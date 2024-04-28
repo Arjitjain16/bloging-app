9cd91c9dcfaf02d9153d73f8f6ef1cb51156643d
Arjitjain16 <arjitvansh16@gmail.com>
2024-04-28 23:09:08 +0530

    jwt becrypt commit

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const env = require('dotenv').config();
-
 const path = require("path")
 const express = require("express")
 const expressLayout = require("express-ejs-layouts")
+const cookieParser = require("cookie-parser")
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 
-const connectDB = require('./server/config/db')
+
+const connectDB = require('./server/config/db');
+const { mongo } = require('mongoose');
 
 const app = express()
 const PORT = 8001 || process.env.PORT
+
 
 //mongo db 
 connectDB()
 
 
+
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
+app.use(cookieParser);
+
+
+app.use(session({
+    secret: 'Blogify',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl : process.env.MONGODB_URL })
+}));
  
 app.use(express.static("public"))
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,9 +2,30 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 const User = require('../models/user');
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 // const layouts = require('layouts');
 
 const adminLayout = "../views/layouts/admin"
+
+jwtSecret = process.env.JWT_SECRET
+
+
+const authMiddleware = (req, res, next)=>{
+    const token = res.cookie.token
+
+    if(!token){
+        res.status(401).json({message : "Unauthorized"})
+    }
+
+    try {
+        const decode = jwt.verify(token, jwtSecret)
+        req.userId = decode.userId
+        next()
+    } catch (error) {
+        res.status(401).json({message : "Unauthorized"})
+    }
+}
 
 /**
  * GET /
@@ -26,19 +47,59 @@ router.get("/admin", function(req, res){
 
 //Post routes for admin and register
 
-router.post("/admin", function(req, res){
+router.post("/admin", async function(req, res){
     try {
         const {username, password} = req.body
-        if (req.body.username === "admin" && req.body.password === "password"){
-            res.send("you are logges in")
+        const user = await User.findOne({username})
+
+        if(!user){
+            return res.status(401).json({message : "Invalid credential"})
         }
-        else {
-            res.send("wrong username or password")
+
+        const PasswordValid = bcrypt.compare(password, user.password)
+
+        if(!PasswordValid){
+            return res.status(401).json({message : "Invalid credentials"})
         }
+
+        const token = jwt.sign({userId : user._id}, jwtSecret)
+        res.cookie('token', token , {httpOnly : true})
+
+        res.redirect("/dashboard")
 
     } catch (error) {
         console.log(error)
     }
 })
+
+// dashboard
+
+router.get('/dashboard', authMiddleware, async function(req, res){
+    res.render('admin/dashboard')
+})
+
+// register 
+
+router.post("/register", async function(req, res){
+    try {
+        const {username, password} = req.body
+        const hashedPassword =  bcrypt(password , 10)
+
+        try {
+            const user = await User.create({username, password:hashedPassword})
+            res.status(201).json({message : "user create", user})
+        } catch (error) {
+            if(error.code === 11000){
+                res.status(401).json({message : "user already exist"})
+            }
+            else{
+                res.status(500).json({message : "Internal server errror"})
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 
 module.exports = router
